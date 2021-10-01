1db409d056c3355a89c9b8d065ef06439822568a
rastabrandy02 <73582888+rastabrandy02@users.noreply.github.com>
2021-10-01 12:48:33 +0200

    Using XML to adjust window, title and audio configuration

---
+++ XML_Config_Exercise/App.cpp
@@ -0,0 +1,274 @@
+#include "App.h"
+#include "Window.h"
+#include "Input.h"
+#include "Render.h"
+#include "Textures.h"
+#include "Audio.h"
+#include "Scene.h"
+
+#include "Defs.h"
+#include "Log.h"
+
+#include <iostream>
+#include <sstream>
+
+// Constructor
+App::App(int argc, char* args[]) : argc(argc), args(args)
+{
+	frames = 0;
+
+	input = new Input();
+	win = new Window();
+	render = new Render();
+	tex = new Textures();
+	audio = new Audio();
+	scene = new Scene();
+
+	// Ordered for awake / Start / Update
+	// Reverse order of CleanUp
+	AddModule(input);
+	AddModule(win);
+	AddModule(tex);
+	AddModule(audio);
+	AddModule(scene);
+
+	// Render last to swap buffer
+	AddModule(render);
+}
+
+// Destructor
+App::~App()
+{
+	// Release modules
+	ListItem<Module*>* item = modules.end;
+
+	while(item != NULL)
+	{
+		RELEASE(item->data);
+		item = item->prev;
+	}
+
+	modules.clear();
+}
+
+void App::AddModule(Module* module)
+{
+	module->Init();
+	modules.add(module);
+}
+
+// Called before render is available
+bool App::Awake()
+{
+	bool ret = false;
+
+	// L01: TODO 3: Load config from XML
+	ret = LoadConfig();
+
+	if(ret == true)
+	{
+		const char* xml_title = configNode.child("app").child("title").child_value();
+		
+		title.create(xml_title); // L01: TODO 4: Read the title from the config file
+		win->SetTitle(title.GetString());
+
+		ListItem<Module*>* item;
+		item = modules.start;
+
+		while(item != NULL && ret == true)
+		{
+			// L01: TODO 5: Add a new argument to the Awake method to receive a pointer to an xml node.
+			// If the section with the module name exists in config.xml, fill the pointer with the valid xml_node
+			// that can be used to read all variables for that module.
+			// Send nullptr if the node does not exist in config.xml
+			pugi::xml_node node;
+			
+			/*if (item->data->name == "window") node = configNode.child("window");
+			if (item->data->name == "renderer") node = configNode.child("renderer");*/
+			node = configNode.child(item->data->name.GetString());
+			
+			ret = item->data->Awake(node);
+			item = item->next;
+		}
+	}
+
+	return ret;
+}
+
+// Called before the first frame
+bool App::Start()
+{
+	bool ret = true;
+	ListItem<Module*>* item;
+	item = modules.start;
+
+	while(item != NULL && ret == true)
+	{
+		ret = item->data->Start();
+		item = item->next;
+	}
+
+	return ret;
+}
+
+// Called each loop iteration
+bool App::Update()
+{
+	bool ret = true;
+	PrepareUpdate();
+
+	if(input->GetWindowEvent(WE_QUIT) == true)
+		ret = false;
+
+	if(ret == true)
+		ret = PreUpdate();
+
+	if(ret == true)
+		ret = DoUpdate();
+
+	if(ret == true)
+		ret = PostUpdate();
+
+	FinishUpdate();
+	return ret;
+}
+
+// Load config from XML file
+bool App::LoadConfig()
+{
+	bool ret = false;
+
+	// L01: TODO 3: Load config.xml file using load_file() method from the xml_document class
+	pugi::xml_parse_result result = configFile.load_file("config.xml");
+	// L01: TODO 3: Check result for loading errors
+	if (result.description() != NULL)
+	{
+		configNode = configFile.child("config");
+		ret = true;
+	}
+	else LOG("Could not charge config.xml %s", result.description());
+	return ret;
+}
+
+// ---------------------------------------------
+void App::PrepareUpdate()
+{
+}
+
+// ---------------------------------------------
+void App::FinishUpdate()
+{
+	
+}
+
+// Call modules before each loop iteration
+bool App::PreUpdate()
+{
+	bool ret = true;
+	ListItem<Module*>* item;
+	item = modules.start;
+	Module* pModule = NULL;
+
+	for(item = modules.start; item != NULL && ret == true; item = item->next)
+	{
+		pModule = item->data;
+
+		if(pModule->active == false) {
+			continue;
+		}
+
+		ret = item->data->PreUpdate();
+	}
+
+	return ret;
+}
+
+// Call modules on each loop iteration
+bool App::DoUpdate()
+{
+	bool ret = true;
+	ListItem<Module*>* item;
+	item = modules.start;
+	Module* pModule = NULL;
+
+	for(item = modules.start; item != NULL && ret == true; item = item->next)
+	{
+		pModule = item->data;
+
+		if(pModule->active == false) {
+			continue;
+		}
+
+		ret = item->data->Update(dt);
+	}
+
+	return ret;
+}
+
+// Call modules after each loop iteration
+bool App::PostUpdate()
+{
+	bool ret = true;
+	ListItem<Module*>* item;
+	Module* pModule = NULL;
+
+	for(item = modules.start; item != NULL && ret == true; item = item->next)
+	{
+		pModule = item->data;
+
+		if(pModule->active == false) {
+			continue;
+		}
+
+		ret = item->data->PostUpdate();
+	}
+
+	return ret;
+}
+
+// Called before quitting
+bool App::CleanUp()
+{
+	bool ret = true;
+	ListItem<Module*>* item;
+	item = modules.end;
+
+	while(item != NULL && ret == true)
+	{
+		ret = item->data->CleanUp();
+		item = item->prev;
+	}
+
+	return ret;
+}
+
+// ---------------------------------------
+int App::GetArgc() const
+{
+	return argc;
+}
+
+// ---------------------------------------
+const char* App::GetArgv(int index) const
+{
+	if(index < argc)
+		return args[index];
+	else
+		return NULL;
+}
+
+// ---------------------------------------
+const char* App::GetTitle() const
+{
+	return title.GetString();
+}
+
+// ---------------------------------------
+const char* App::GetOrganization() const
+{
+	return organization.GetString();
+}
+
+
+
+
